b4ba56f8207f94e318ffc5bae702b134d97c6cf1
Mihail <mihailzharikow@yandex.ru>
2025-11-09 18:59:19 +0700

    feat(display): introduce display_t struct
    Adds the core `display_t` struct, which contains function pointers for
    different display methods. This struct will be the base for making other
    structs displayable

--- display.h
+++ display.h
@@ -4,6 +4,22 @@ TODO:
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#include <stddef.h>
+#include <stdio.h>
+
+/// @brief The base struct for containing pointers to display functions.
+/// @note  For a struct to be displayable, it must:
+/// - Have a display_t member as its first field
+/// - Have a valid pointer to itself in the display_t::self field
+/// - Implement at least one of display_fn, fdisplay_fn, or sndisplay_fn
+typedef struct display_t {
+  int (*display_fn)(const void *);
+  int (*fdisplay_fn)(const void *, FILE *);
+  int (*sndisplay_fn)(const void *, char *, size_t);
+  void *self;
+
+} display_t;
+
 // TODO:
 
 #endif // DISPLAY_H
